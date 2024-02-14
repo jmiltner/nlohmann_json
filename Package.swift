dfa6f2da75bdb6782164598933620655da1676b0
Jens <jmiltner@users.noreply.github.com>
2024-02-14 12:25:50 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
         .iOS(.v12), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v4)
     ],
     products: [
-        .library(name: "json", targets: ["nlohmann_json"])
+        .library(name: "nlohmann_json", targets: ["json"])
     ],
     targets: [
         .target(
-            name: "nlohmann_json",
+            name: "json",
             path: "single_include/nlohmann",
             publicHeadersPath: "."
         )
